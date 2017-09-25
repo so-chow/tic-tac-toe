cea6474593ea15f75346289f0ebcbb1f36d076d9
Sophia Chow <Sophia.t.chow@gmail.com>
2017-09-25 14:45:27 -0500

    added functionality

--- js/js.js
+++ js/js.js
@@ -6,11 +6,13 @@ var winConditions = [[0,1,2], [3,4,5], [6,7,8],
                        [0,4,8], [6,4,2]]
 var gameOver;
 var counter;
-
+var o_moves = [];
+var x_moves = [];
 // $(document).ready(function(){
 
-  var boxes = [...document.getElementsByClassName('tic-tac-button')];
-
+var boxes = [...document.getElementsByClassName('tic-tac-button')];
+var reset_button = document.querySelector("#reset");
+reset_button.addEventListener("click", reset);
   // if winner === Player O start counter at 0
   if (winner === "Player O"){
     counter = 0;
@@ -21,196 +23,270 @@ var counter;
 
 
   boxes.forEach(function(box){
-    box.addEventListener('click', function(event){
+    box.addEventListener('click', function onClick(event){
       //add O's are when count is even and X when it's odd
       console.log(counter);
-      if (counter % 2 === 0){
-        box.innerText = "O";
-        box.className += ' playerO';
-      } else {
-        box.innerText = "X";
-        box.className += ' playerX';
+      if(box.innerText!=''){
+        // event.preventDefault();
+        document.querySelector(".already").style.display = "block";
+      }else{
+        document.querySelector(".already").style.display = "none";
+        if (counter % 2 === 0){ //if counter is EVEN then put O
+          box.innerText = "O";
+          box.className += ' playerO'; //adding class name "player O" to this box
+          o_moves.push(boxes.indexOf(box));
+          if(checkWin(o_moves)){
+            document.getElementsByClassName("messageO")[0].style.display = "block";
+            fillSpace();
+          }
+
+          //TO DO check for win
+        } else { //if counter is ODD put X
+          box.innerText = "X";
+          box.className += ' playerX'; //adding class name "player X" to this box
+          x_moves.push(boxes.indexOf(box));
+          if(checkWin(x_moves)){
+            document.getElementsByClassName("messageX")[0].style.display = "block";
+            fillSpace();
+          }
+          //TO DO check for win
+        }
+        // increase count by 1
+        counter++;
       }
-      // increase count by 1
-      counter++;
     })
   });
 
-
-  var x = "x";
-  var o = "o";
-  var count = 0; //O's are when count is even and X when it's odd
-  var o_win = 0;
-  var x_win = 0;
-  $(".tic-tac-button").click(function(){
-
-    if($("#one").hasClass("o") &&
-      $("#two").hasClass("o") &&
-      $("#three").hasClass("o") ||
-      $("#four").hasClass("o") &&
-      $("#five").hasClass("o") &&
-      $("#six").hasClass("o") ||
-      $("#seven").hasClass("o") &&
-      $("#eight").hasClass("o") &&
-      $("nine").hasClass("o") ||
-      $("#one").hasClass('o') &&
-      $("#four").hasClass('o') &&
-      $("#seven").hasClass('o') ||
-      $("#two").hasClass('o') &&
-      $("#five").hasClass('o') &&
-      $("#eight").hasClass('o') ||
-      $("#three").hasClass('o') &&
-      $("#six").hasClass('o') &&
-      $("#nine").hasClass('o') ||
-      $("#one").hasClass('o') &&
-      $("#five").hasClass('o') &&
-      $("#nine").hasClass('o') ||
-      $("#three").hasClass('o') &&
-      $("#five").hasClass('o') &&
-      $("#seven").hasClass('o'))
-        {
-          $(".messageX").hide();
-          $(".messageO").show();
-          winner = "Player O";
-          $(".tic-tac-button").text(" ");
-          $(".tic-tac-button").removeClass("disable");
-          $(".tic-tac-button").removeClass('o');
-          $(".tic-tac-button").removeClass('x');
-        }
-    else if($("#one").hasClass("x") &&
-      $("#two").hasClass('x') &&
-      $("#three").hasClass('x') ||
-      $("#four").hasClass('x') &&
-      $("#five").hasClass('x') &&
-      $("#six").hasClass('x') ||
-      $("#seven").hasClass('x') &&
-      $("#eight").hasClass('x') &&
-      $("#nine").hasClass('x') ||
-      $("#one").hasClass('x') &&
-      $("#four").hasClass('x') &&
-      $("#seven").hasClass('x') ||
-      $("#two").hasClass('x') &&
-      $("#five").hasClass('x') &&
-      $("#eight").hasClass('x') ||
-      $("#three").hasClass('x') &&
-      $("#six").hasClass('x') &&
-      $("#nine").hasClass('x') ||
-      $("#one").hasClass('x') &&
-      $("#five").hasClass('x') &&
-      $("#nine").hasClass('x') ||
-      $("#three").hasClass('x') &&
-      $("#five").hasClass('x') &&
-      $("#seven").hasClass('x'))
-        {
-          $(".messageO").hide();
-          $(".messageX").show();
-          $(".tic-tac-button").text(" ");
-          $(".tic-tac-button").removeClass("disable");
-          $(".tic-tac-button").removeClass('o');
-          $(".tic-tac-button").removeClass('x');
-        }
-    else if (count === 9){
-      $(".alert").show();
-      $(".tic-tac-button").text(" ");
-      $(".tic-tac-button").removeClass('disable');
-      $(".tic-tac-button").removeClass('o');
-      $(".tic-tac-button").removeClass('x');
-      count = 0;
-       }
-    else if ($(this).hasClass('disable'))
-      {
-        $(".already").show();
-      }
-    else if (count%2 ===0){
-      count++
-      $(this).text(x);
-      $(this).addClass("disable x");
-
-        if($("#one").hasClass('x') &&
-        $("#two").hasClass('x') &&
-        $("#three").hasClass('x') ||
-        $("#four").hasClass('x') &&
-        $("#five").hasClass('x') &&
-        $("#six").hasClass('x') ||
-        $("#seven").hasClass('x') &&
-        $("#eight").hasClass('x') &&
-        $("#nine").hasClass('x') ||
-        $("#one").hasClass('x') &&
-        $("#four").hasClass('x') &&
-        $("#seven").hasClass('x') ||
-        $("#two").hasClass('x') &&
-        $("#five").hasClass('x') &&
-        $("#eight").hasClass('x') ||
-        $("#three").hasClass('x') &&
-        $("#six").hasClass('x') &&
-        $("#nine").hasClass('x') ||
-        $("#one").hasClass('x') &&
-        $("#five").hasClass('x') &&
-        $("#nine").hasClass('x') ||
-        $("#three").hasClass('x') &&
-        $("#five").hasClass('x') &&
-        $("#seven").hasClass('x'))
-          {
-           $(".messageO").hide();
-           $(".messageX").show();
-           count = 0;
-           x_win++;
-           $('#x_win').text(x_win);
-          }
+function checkWin(moves){
+  var player = "";
+  if(counter%2 === 0){
+    player = "o";
+  }
+  else{
+    player = "x";
+  }
+  for(var i = 0; i<winConditions.length; i++){
+    var el = winConditions[i];
+    if(moves.includes(el[0]) && moves.includes(el[1]) && moves.includes(el[2])){
+      return true;
     }
-    else {
-      count++
-      $(this).text(o);
-      $(this).addClass("disable o");
-      if ($("#one").hasClass('o') &&
-        $("#two").hasClass('o') &&
-        $("#three").hasClass('o') ||
-        $("#four").hasClass('o') &&
-        $("#five").hasClass('o') &&
-        $("#six").hasClass('o') ||
-        $("#seven").hasClass('o') &&
-        $("#eight").hasClass('o') &&
-        $("#nine").hasClass('o') ||
-        $("#one").hasClass('o') &&
-        $("#four").hasClass('o') &&
-        $("#seven").hasClass('o') ||
-        $("#two").hasClass('o') &&
-        $("#five").hasClass('o') &&
-        $("#eight").hasClass('o') ||
-        $("#three").hasClass('o') &&
-        $("#six").hasClass('o') &&
-        $("#nine").hasClass('o') ||
-        $("#one").hasClass('o') &&
-        $("#five").hasClass('o') &&
-        $("#nine").hasClass('o') ||
-        $("#three").hasClass('o') &&
-        $("#five").hasClass('o') &&
-        $("#seven").hasClass('o'))
-          {
-            $(".messageX").hide();
-            $(".messageO").show();
-            count = 0;
-            o_win++
-            $('#o_win').text(o_win)
-            winner = "Player O"
-            }
-        }
-     });
+  }
+  return false;
+}
 
-  $("#reset").click(function () {
-    $(".tic-tac-button").text(" ");
-    $(".tic-tac-button").removeClass('disable');
-    $(".tic-tac-button").removeClass('o');
-    $(".tic-tac-button").removeClass('x');
-    $(".messageO").hide();
-    $(".messageX").hide();
-    $(".alert").hide();
-    count = 0;
-    counter = 0;
-  });
+function reset(){
+  boxes.forEach(function(box){
+    box.innerText = "";
+  })
+  document.querySelector(".already").style.display = "none";
+  document.querySelector(".messageO").style.display = "none";
+  document.querySelector(".messageX").style.display = "none";
+  o_moves = [];
+  x_moves =[];
+  counter = 1;
+}
+
+function fillSpace(){
+  boxes.forEach(function(box){
+    if(box.innerText ===""){
+      box.innerText = "*";
+      box.style.color = "#7ACDBE";
+    }
+  })
+}
+
+
+
+  // if(counter%2 === 0){
+  //   player = "o";
+  // }
+  // else{
+  //   player = "x";
+  // }
+  // winConditions.forEach(function(el){
+  //  if(moves.includes(el[0]) && moves.includes(el[1]) && moves.includes(el[2])){
+  //   return true;
+  //   console.log(player + " wins!");
+  //   }
+  // })
+  //return false;
+// }
+
+//JQUERY CODE
+  // var x = "x";
+  // var o = "o";
+  // var count = 0; //O's are when count is even and X when it's odd
+  // var o_win = 0;
+  // var x_win = 0;
+  // $(".tic-tac-button").click(function(){
+
+  //   if($("#one").hasClass("o") &&
+  //     $("#two").hasClass("o") &&
+  //     $("#three").hasClass("o") ||
+  //     $("#four").hasClass("o") &&
+  //     $("#five").hasClass("o") &&
+  //     $("#six").hasClass("o") ||
+  //     $("#seven").hasClass("o") &&
+  //     $("#eight").hasClass("o") &&
+  //     $("nine").hasClass("o") ||
+  //     $("#one").hasClass('o') &&
+  //     $("#four").hasClass('o') &&
+  //     $("#seven").hasClass('o') ||
+  //     $("#two").hasClass('o') &&
+  //     $("#five").hasClass('o') &&
+  //     $("#eight").hasClass('o') ||
+  //     $("#three").hasClass('o') &&
+  //     $("#six").hasClass('o') &&
+  //     $("#nine").hasClass('o') ||
+  //     $("#one").hasClass('o') &&
+  //     $("#five").hasClass('o') &&
+  //     $("#nine").hasClass('o') ||
+  //     $("#three").hasClass('o') &&
+  //     $("#five").hasClass('o') &&
+  //     $("#seven").hasClass('o'))
+  //       {
+  //         $(".messageX").hide();
+  //         $(".messageO").show();
+  //         winner = "Player O";
+  //         $(".tic-tac-button").text(" ");
+  //         $(".tic-tac-button").removeClass("disable");
+  //         $(".tic-tac-button").removeClass('o');
+  //         $(".tic-tac-button").removeClass('x');
+  //       }
+  //   else if($("#one").hasClass("x") &&
+  //     $("#two").hasClass('x') &&
+  //     $("#three").hasClass('x') ||
+  //     $("#four").hasClass('x') &&
+  //     $("#five").hasClass('x') &&
+  //     $("#six").hasClass('x') ||
+  //     $("#seven").hasClass('x') &&
+  //     $("#eight").hasClass('x') &&
+  //     $("#nine").hasClass('x') ||
+  //     $("#one").hasClass('x') &&
+  //     $("#four").hasClass('x') &&
+  //     $("#seven").hasClass('x') ||
+  //     $("#two").hasClass('x') &&
+  //     $("#five").hasClass('x') &&
+  //     $("#eight").hasClass('x') ||
+  //     $("#three").hasClass('x') &&
+  //     $("#six").hasClass('x') &&
+  //     $("#nine").hasClass('x') ||
+  //     $("#one").hasClass('x') &&
+  //     $("#five").hasClass('x') &&
+  //     $("#nine").hasClass('x') ||
+  //     $("#three").hasClass('x') &&
+  //     $("#five").hasClass('x') &&
+  //     $("#seven").hasClass('x'))
+  //       {
+  //         $(".messageO").hide();
+  //         $(".messageX").show();
+  //         $(".tic-tac-button").text(" ");
+  //         $(".tic-tac-button").removeClass("disable");
+  //         $(".tic-tac-button").removeClass('o');
+  //         $(".tic-tac-button").removeClass('x');
+  //       }
+  //   else if (count === 9){
+  //     $(".alert").show();
+  //     $(".tic-tac-button").text(" ");
+  //     $(".tic-tac-button").removeClass('disable');
+  //     $(".tic-tac-button").removeClass('o');
+  //     $(".tic-tac-button").removeClass('x');
+  //     count = 0;
+  //      }
+  //   else if ($(this).hasClass('disable'))
+  //     {
+  //       $(".already").show();
+  //     }
+  //   else if (count%2 ===0){
+  //     count++
+  //     $(this).text(x);
+  //     $(this).addClass("disable x");
+
+  //       if($("#one").hasClass('x') &&
+  //       $("#two").hasClass('x') &&
+  //       $("#three").hasClass('x') ||
+  //       $("#four").hasClass('x') &&
+  //       $("#five").hasClass('x') &&
+  //       $("#six").hasClass('x') ||
+  //       $("#seven").hasClass('x') &&
+  //       $("#eight").hasClass('x') &&
+  //       $("#nine").hasClass('x') ||
+  //       $("#one").hasClass('x') &&
+  //       $("#four").hasClass('x') &&
+  //       $("#seven").hasClass('x') ||
+  //       $("#two").hasClass('x') &&
+  //       $("#five").hasClass('x') &&
+  //       $("#eight").hasClass('x') ||
+  //       $("#three").hasClass('x') &&
+  //       $("#six").hasClass('x') &&
+  //       $("#nine").hasClass('x') ||
+  //       $("#one").hasClass('x') &&
+  //       $("#five").hasClass('x') &&
+  //       $("#nine").hasClass('x') ||
+  //       $("#three").hasClass('x') &&
+  //       $("#five").hasClass('x') &&
+  //       $("#seven").hasClass('x'))
+  //         {
+  //          $(".messageO").hide();
+  //          $(".messageX").show();
+  //          count = 0;
+  //          x_win++;
+  //          $('#x_win').text(x_win);
+  //         }
+  //   }
+  //   else {
+  //     count++
+  //     $(this).text(o);
+  //     $(this).addClass("disable o");
+  //     if ($("#one").hasClass('o') &&
+  //       $("#two").hasClass('o') &&
+  //       $("#three").hasClass('o') ||
+  //       $("#four").hasClass('o') &&
+  //       $("#five").hasClass('o') &&
+  //       $("#six").hasClass('o') ||
+  //       $("#seven").hasClass('o') &&
+  //       $("#eight").hasClass('o') &&
+  //       $("#nine").hasClass('o') ||
+  //       $("#one").hasClass('o') &&
+  //       $("#four").hasClass('o') &&
+  //       $("#seven").hasClass('o') ||
+  //       $("#two").hasClass('o') &&
+  //       $("#five").hasClass('o') &&
+  //       $("#eight").hasClass('o') ||
+  //       $("#three").hasClass('o') &&
+  //       $("#six").hasClass('o') &&
+  //       $("#nine").hasClass('o') ||
+  //       $("#one").hasClass('o') &&
+  //       $("#five").hasClass('o') &&
+  //       $("#nine").hasClass('o') ||
+  //       $("#three").hasClass('o') &&
+  //       $("#five").hasClass('o') &&
+  //       $("#seven").hasClass('o'))
+  //         {
+  //           $(".messageX").hide();
+  //           $(".messageO").show();
+  //           count = 0;
+  //           o_win++
+  //           $('#o_win').text(o_win)
+  //           winner = "Player O"
+  //           }
+  //       }
+  //    });
+
+  // $("#reset").click(function () {
+  //   $(".tic-tac-button").text(" ");
+  //   $(".tic-tac-button").removeClass('disable');
+  //   $(".tic-tac-button").removeClass('o');
+  //   $(".tic-tac-button").removeClass('x');
+  //   $(".messageO").hide();
+  //   $(".messageX").hide();
+  //   $(".alert").hide();
+  //   count = 0;
+  //   counter = 0;
+  // });
+
 
 // });
-
 //1st player clicks on a space
 //an X appears
 //2nd player clicks on a space
